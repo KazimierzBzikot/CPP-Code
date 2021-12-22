8903377c4049bf178dfad00b0378dd6aaa4721be
Kaz Bzikot <kazzbzikot@outlook.com>
2021-12-22 02:11:29 +0000

    idk

---
+++ temp_funcs.cpp
@@ -0,0 +1,51 @@
+/*
+Functions can be overloaded; A function name can be used twice with different variable types,
+and act as though the seperate functions are defined with different names. The following code is legal:
+
+// overloading functions
+#include <iostream>
+using namespace std;
+
+int operate (int a, int b)
+{
+  return (a*b);
+}
+
+double operate (double a, double b)
+{
+  return (a/b);
+}
+
+int main ()
+{
+  int x=5,y=2;
+  double n=5.0,m=2.0;
+  cout << operate (x,y) << '\n';
+  cout << operate (n,m) << '\n';
+  return 0;
+}
+
+And will return 10 and 2.5
+*/
+// for functions which we want to take different parameter types we can use function templates:
+// function template
+#include <iostream>
+using namespace std;
+
+template <class T>
+T sum (T a, T b)
+{
+  T result;
+  result = a + b;
+  return result;
+}
+
+int main () {
+  int i=5, j=6, k;
+  double f=2.0, g=0.5, h;
+  k=sum<int>(i,j);
+  h=sum<double>(f,g);
+  cout << k << '\n';
+  cout << h << '\n';
+  return 0;
+}
